61f4d869826d67e0da18d6adbee293b3de686c03
Štěpán Zechner <stepan@zechner.cz>
2022-09-11 17:27:30 +0200

    Resolve conflicts

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ echo "\nHello.\n"
 
 npm run build
 
-cf login -a $1 -o $2 -u $3  -p $4  -s $5
+cf login -a "$1" -o "$2" -u "$3"  -p "$4"  -s "$5"
 
-cf deploy $6 --retries 1
+cf deploy "$6" --retries 1
 
 cf logout
