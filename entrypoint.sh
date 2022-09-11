3dcf4b3438f1c6aa42bb1075f3cfd56145cba40f
Štěpán Zechner <stepan@zechner.cz>
2022-09-11 16:04:37 +0200

    Fix deploy issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -l
 
 echo "\nHello.\n"
-cf login -a "$1" -o "$2" -u "$3" -p "$4" -s "$5"
 
-npm install && npm run build && npm run deploy
+npm run build
+
+cf login -a $1 -o $2 -u $3  -p $4  -s $5
+cf deploy mta_archives/archive.mtar --retries 1
 
 cf logout
